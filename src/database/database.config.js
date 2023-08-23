f3ccaf20d2332501100506e866a0e47b179810d5
Lucas Sulivan <lucas.sulivan@hotmail.com>
2023-08-23 14:55:33 +0100

    create connection with database

---
+++ src/database/database.config.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2/promise")
+require("dotenv").config()
+
+
+const database = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+    database
+    .getConnection()
+    .then(()=>{
+        console.log("Data base Running")
+    })
+    .catch((error)=>{
+        console.error(error)
+    });
+ 
+    module.exports = database
