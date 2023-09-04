3a9f4c6af39ee54a2c575ee0e66c249b14b2a9aa
LuisC92 <luis.clara@wildcodeschool.com>
2023-09-04 11:52:58 +0100

    add generateRangeDays model and add it on the new trip controller

--- src/models/new-trip.model.js
+++ src/models/new-trip.model.js
@@ -7,33 +7,30 @@ const createNewTrip =(trip)=>{
         .then(([results])=> results);
 }
 
+const generateDateRange = async(startDate, endDate, tripId) => {
+    let currentDate = new Date(startDate);
+    const endDateObj = new Date(endDate);
+    
+    try {
+      while (currentDate <= endDateObj) {
+        const formattedDate = currentDate.toISOString().split('T')[0];
+  
+        await database.execute('INSERT INTO number_days (day, trip_id) VALUES (?, ?)', [
+          formattedDate,
+          tripId,
+        ]);
+  
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    } catch (error) {
+      throw error;
+    }
+  
+    return 1;
+  }
+  
+
 module.exports={
-    createNewTrip
+    createNewTrip,
+    generateDateRange,
 }
-
-
-// CREATE FUNCTION generate_date_range(
-//     start_date DATE,
-//     end_date DATE,
-//     trip_id INT
-// ) RETURNS INT READS SQL DATA BEGIN WHILE start_date <= end_date DO
-// INSERT INTO
-//     number_days (day, trip_id)
-// VALUES
-//     (start_date, trip_id);
-
-// SET
-//     start_date = DATE_ADD(start_date, INTERVAL 1 DAY);
-
-// END WHILE;
-
-// RETURN 1;
-
-// END;
-
-// -- Call the function for each trip
-// SELECT
-//     generate_date_range('2023-09-01', '2023-09-07', 1);
-
-// SELECT
-//     generate_date_range('2023-08-15', '2023-08-21', 2);
